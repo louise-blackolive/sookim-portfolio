a66659abe81d3ecf240cf70b18c75ee4c900afab
sookim <louise.blackolive@gmail.com>
2020-03-03 18:01:21 +0900

    smooth scroll

--- js/navBar.js
+++ js/navBar.js
@@ -1,5 +1,30 @@
-const port = document.querySelector(".js-scroll-portfolio");
+const navIcon = document.querySelector(".top-bar"),
+  clickIcon = navIcon.querySelectorAll("a");
 
-function init() {}
+console.log(clickIcon);
+
+function moveWindow(event) {
+  var destId = clickIcon.id;
+  console.log(destId);
+  if (destId === "js-scroll-portfolio") {
+    window.scrollTo({
+      top: 900,
+      behavior: "smooth"
+    });
+  } else if (destId === "js-scroll-about") {
+    window.scrollTo({
+      top: 1000,
+      behavior: "smooth"
+    });
+  }
+}
+
+function init() {
+  for (var i = 0; i < 3; i++) {
+    destId = clickIcon[i].id;
+    console.log(destId);
+    clickIcon[i].onclick = moveWindow();
+  }
+}
 
 init();
